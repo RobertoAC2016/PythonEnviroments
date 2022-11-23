762bd1e65c05d6fd30d3b77769c5ea2182db20f5
Roberto Avila <ravila@dealergeek.com>
2022-11-22 23:54:37 -0600

    Final

---
+++ app.py
@@ -0,0 +1,47 @@
+
+
+print("Hola mundo virtual env")
+
+
+
+# En este tutorial vamos a ver como crear un ambiente virtual en Python, de este modo podremos replicar, borrar, agregar y hacer todo lo necesario sin afectar el sistema principal o proyecto
+
+# Como se hace eso
+
+# Bueno vamos a crearlo usando una sentencia q incluye el comando venv
+
+# El comando es
+# python -m         <- Este es el comando que usa el programa python
+# venv              <- este es el comando que reconoce python para generar un ambiente virtual
+# rac               <- Este es el nombre de la carpeta q se generara dentro del sistema de archivos del proyecto de python
+# Esto se ejecuta dentro de la linea de comando de consola
+
+# para ejemplificarlo veremos la version de python q esta corriendo en el sistema y despues de la virtualizacion, estaremos usando la version del ambiente virtualizado
+
+# Usaremos la version 3.9 para el ambiente virtual que este disponible en la rura python39
+
+
+# La forma de invocarlo es usando el comando en el caso de windows la siguiente linea
+
+# Primero la carpeta
+# Seguido de scripts/activate
+
+# activate es el comando q inicia la virtualizacion y para salir es todo igual pero al final se usa deactivate
+
+# Veamos como funciona
+
+
+
+# Calidemos ahora preguntando por la version de python
+
+
+# El proceso de virtualizacion del proyecto de python ha concluido
+
+
+# la ventaja, nuevamente, es no mezclar versiones de librerias entre proyectos, usando en cada proyecto, las versiones que son necesarios para cada solucion
+
+# desactivemos la virtualizacion
+
+
+# y con esto concluimos el tema, y nuestra app de python que imprime mensaje sigue corriendo sin problema
+
